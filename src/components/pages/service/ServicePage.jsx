31293e60e8ad83c84cada4147e31dbe6b85c6ad9
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-08-28 15:18:37 +0900

    feat: added router function

---
+++ src/components/pages/service/ServicePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function ServicePage() {
+    return (
+        <ServicePageContainer>
+            <p>ServicePage</p>
+        </ServicePageContainer>
+    );
+}
+
+const ServicePageContainer = styled.main`
+background-color: white;
+`;
+
+export default ServicePage;
---
+++ src/pages/faq.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FaqPage from '../components/pages/faq/FaqPage';
+import DefaultLayout from '../components/layout/DefaultLayout';
+
+function Faq() {
+    return (
+        <DefaultLayout>
+            <FaqPage />
+        </DefaultLayout>
+    );
+}
+
+export default Faq;
