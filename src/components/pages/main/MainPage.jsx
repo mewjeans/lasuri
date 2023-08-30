8aab2a170a1514a2248ad88ea32f5084b22473ec
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-08-30 15:20:26 +0900

    feat: added main component and image

--- src/components/pages/main/MainPage.jsx
+++ src/components/pages/main/MainPage.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 // import Paths from "../../../constant/path";
 // import Fonts from "../../../styles/fonts";
-
+import mainImage from '../../../assets/main-image-2.jpg';
 
 function MainPage() {
     return (
         <Main>
-            <p>This is Main</p>
+            <div>
+                <img src={mainImage} alt='mainimage' />
+            </div>
         </Main>
     );
 }
 
 const Main = styled.main`
     background-color: white;
+    width: 120rem;
+
+    > div {
+        width: 120rem;
+
+        > img {
+            width: 100%;
+            height: 50rem;
+            overflow: hidden;
+        }
+    }
 `;
 
 export default MainPage;
